336ec195691bfa35826bb08c95769a5d88c9cebc
Rajskiller <47172445+Rajskiller@users.noreply.github.com>
2021-04-26 14:26:23 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include "gd32vf103.h"
+#include "drivers.h"
+#include "dac.h"
+#include "pwm.h"
+
+int main(void){
+    int speeda=0,speedb=0;
+    t5omsi();                               // Initialize timer5 1kHz
+
+    T1powerUpInitPWM(0xD);
+    while (1)
+    {
+        if(t5expq())
+        {
+            T1setPWMmotorB(speedb=+10000);
+            T1setPWMmotorA(speeda=+10000);
+            
+        }
+        
+    }
+}
---
+++ src/pwm.h
@@ -0,0 +1,8 @@
+#include "gd32vf103.h"
+void T1powerUpInitPWM(uint16_t ch);
+
+void T1setPWMch2(int value);
+
+void T1setPWMmotorA(int throttel);
+
+void T1setPWMmotorB(int throttel);
